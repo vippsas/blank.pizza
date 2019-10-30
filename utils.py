6f41e28822a9d84e3c57774b521e4153bb6f6fd9
Michael Eikeland <michael.eikeland@blank.no>
2019-10-30 16:25:56 +0100

    Add formatting utils

---
+++ utils.py
@@ -0,0 +1,15 @@
+def create_mention(user_id):
+    return f"<@{user_id}>"
+
+
+def create_mentions(user_ids):
+    users = list(map(create_mention, user_ids))
+    if len(users) == 1:
+        return users[0]
+    if len(users) == 2:
+        return f"{users[0]} og {users[1]}"
+    return f"{', '.join(users[:-1])} og {users[-1]}"
+
+
+def sane_time(dt):
+    return dt.strftime('%d/%m %H:%M')
